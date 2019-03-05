7abfaa73c722a2540a6bebf495229d4cac09fe01
JeanAmodeo <34283042+JeanAmodeo@users.noreply.github.com>
2019-03-05 17:09:14 +0000

    Update game.js

--- www/js/game.js
+++ www/js/game.js
@@ -14,16 +14,43 @@ var config = {
 
 var interval = 0;
 //interval is the cooldwon time between shots
+
+var invul = 0;
+
 var bullets;
 //bullets is a group that contains all the Bullet entities in the game
 var asteroids;
 //asteroids is a group that contains all the Asteroid entities in the game
 
 var astroPresets = {
-    astroLarge: { sprite: 'astroL', maxSpeed:100, score: 20, nextSize: "astroMedium", parts: 2 },
-    astroMedium: { sprite: 'astroM', score: 50,maxSpeed:150,  nextSize: "astroSmall", parts: 2 },
-    astroSmall: {sprite: 'astroS',maxSpeed:200, score: 100 },
-}; 
+    astroLarge: {
+        sprite: 'astroL',
+        minSpeed: 50,
+        maxSpeed: 100,
+        score: 20,
+        nextSize: "astroMedium",
+        parts: 2
+    },
+    astroMedium: {
+        sprite: 'astroM',
+        score: 50,
+        minSpeed: 100,
+        maxSpeed: 150,
+        nextSize: "astroSmall",
+        parts: 2
+    },
+    astroSmall: {
+        sprite: 'astroS',
+        minSpeed: 150,
+        maxSpeed: 200,
+        score: 100
+    },
+};
+
+var gSize = "astroLarge";
+var lives = 3;
+
+// console.log(gSize)
 
 let game = new Phaser.Game(config);
 
@@ -69,6 +96,7 @@ var Bullet = new Phaser.Class({
     },
 
     kill: function () {
+        // console.log(this);
         //function call when bullets attain lifespan
         this.setActive(false);
         this.setVisible(false);
@@ -78,37 +106,43 @@ var Bullet = new Phaser.Class({
 
 var Asteroid = new Phaser.Class({
     //Asteroid class
-    Extends: Phaser.Physics.Arcade.Sprite,
+    Extends: Phaser.Physics.Arcade.Image,
 
     initialize:
 
-        function Asteroid(scene,size) {
-            
-            if (size === undefined || size == 0){size = "astroLarge"};
-            
-            Phaser.Physics.Arcade.Sprite.call(this, scene, 0, 0, astroPresets[size].sprite);
-            this.size = size;
-            this.score = astroPresets[size].score;
-            this.nextSize = astroPresets[size].nextSize;
-            this.parts = astroPresets[size].parts;
+        function Asteroid(scene) {
+            // console.log(gSize);
+            this.size = gSize;
+            // if (this.size === undefined || this.size == 0){this.size = "astroLarge"};
 
-            this.speed = astroPresets[size].maxSpeed;
+            Phaser.Physics.Arcade.Sprite.call(this, scene, 0, 0, astroPresets[this.size].sprite);
+            // console.log(this.size)
+            this.score = astroPresets[this.size].score;
+            this.nextSize = astroPresets[this.size].nextSize;
+            this.parts = astroPresets[this.size].parts;
+            this.speed = astroPresets[this.size].maxSpeed;
         },
 
-    spawn: function (x,y,size) {
-        if (x===undefined){x = Phaser.Math.Between(520, 600)};
-        if (y===undefined){y = Phaser.Math.Between(520, 600)};
+    spawn: function (x, y, size) {
+        // console.log(this.size);
+        if (x === undefined) {
+            x = Phaser.Math.Between(520, 600)
+        };
+        if (y === undefined) {
+            y = Phaser.Math.Between(520, 600)
+        };
 
-        var pos = [x,y];
-        // this.size = size;
-
+        var pos = [x, y];
+        // console.log(this);
+        gSize = size;
+        // console.log(gSize);
         this.setActive(true);
         this.setVisible(true);
         this.setPosition(pos[0], pos[1]);
 
         this.rotation = Phaser.Math.Between(0, 180);
         //console.log( this.size)
-        this.speed = Phaser.Math.Between(80, astroPresets[this.size].maxSpeed);
+        this.speed = Phaser.Math.Between(astroPresets[this.size].minSpeed, astroPresets[this.size].maxSpeed);
         // this.speed = 80;
 
         var angle = Phaser.Math.RND.angle();
@@ -116,16 +150,26 @@ var Asteroid = new Phaser.Class({
         this.scene.physics.velocityFromRotation(angle, this.speed, this.body.velocity);
     },
     update: function (time, delta) {
-        this.rotation+=0.01;
+        this.rotation += 0.01;
     },
 
     kill: function () {
         //function call when asteroids die
+
+        for (var i = 0; i < astroPresets[this.size].parts; i++) {
+            console.log(this.size);
+            // console.log(astroPresets[this.size].nextSize)
+            this.scene.spawnAstro(this.x, this.y, astroPresets[this.size].nextSize);
+        }
+
+
+        this.body.stop();
         this.setActive(false);
         this.setVisible(false);
-        this.body.stop();
-        for (var i = 0; i <astroPresets[this.size].parts; i++) {
-        this.scene.spawnAstro(this.x,this.y, astroPresets[this.size].nextSize);}
+        this.destroy();
+        // console.log(this)
+
+
     }
 })
 
@@ -146,6 +190,7 @@ scene.create = function () {
     scene.cameras.main.setBackgroundColor('#000000')
     //sets scene background to black
     this.ship = this.physics.add.sprite(this.sys.game.config.width / 2, this.sys.game.config.height / 2, 'ship');
+    this.ship.alive = true;
     // bullets = this.physics.add.group({
     //     key: 'bullet',
     //     repeat: 30,
@@ -164,7 +209,7 @@ scene.create = function () {
     asteroids = this.physics.add.group({
         //creates a group of asteroids objects
         classType: Asteroid,
-        maxSize: 60,
+        maxSize: 30,
         runChildUpdate: true
     });
 
@@ -179,9 +224,10 @@ scene.create = function () {
 
     //check collisions between asteroids and bullets
     this.physics.add.overlap(bullets, asteroids, this.astroHit, this.checkHit, this);
+    this.physics.add.overlap(this.ship, asteroids, this.shipHit, this.checkHit, this);
 
     for (var i = 0; i < 6; i++) {
-        this.spawnAstro('','',"AstroLarge");
+        this.spawnAstro('', '', "astroLarge");
     }
 }
 
@@ -192,6 +238,12 @@ scene.update = function (time) {
     this.physics.world.wrap(asteroids, 20);
     //wrap function enables border traversal for the ship, asteroids and bullets, with an offset of 5
     // this.ship.body.setVelocity(0);
+
+    if (!this.ship.alive) {
+        this.ship.setVisible(false);
+    } else {
+        this.ship.setVisible(true);
+    }
 
     if (this.cursors.right.isDown) {
         this.ship.setAngularVelocity(300);
@@ -219,20 +271,38 @@ scene.update = function (time) {
             interval = time + 100;
         }
     }
+    // console.log(this.ship.alive);
+
+    if (!this.ship.alive && time > invul) {
+        this.ship.alive = true;
+        invul = time + 5000;
+
+    }
 };
 
-scene.checkHit = function (bullet, astro) {
-    return (bullet.active && astro.active);
+scene.checkHit = function (obj1, obj2) {
+    return (obj1.active && obj2.active);
 }
 
 scene.astroHit = function (bullet, astro) {
     bullet.kill();
     astro.kill();
 }
+scene.shipHit = function (ship, astro) {
+    astro.kill();
+    if (ship.alive) {
+        ship.alive = false;
+        // console.log("DED XD");
+        lives--;
+        
+    };
 
-scene.spawnAstro = function (x,y,size) {
+}
+
+scene.spawnAstro = function (x, y, size) {
+    // console.log(size)
     var as = asteroids.get();
     if (as) {
-        as.spawn(x,y,size);
+        as.spawn(x, y, size);
     };
 }
